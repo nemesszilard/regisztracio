9b3fcef3584cf79773b12022462a38e31f4fbd8d
nemesszilard <nemes.sziszi@gmail.com>
2022-08-15 17:43:39 +0200

    logika

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const button = document.getElementById("gomb");
-button.disabled = true;
 var email1 = document.getElementById("inputEmail1");
 var email2 = document.getElementById("inputEmail2");
 var jelszo1 = document.getElementById("inputPassword1");
 var jelszo2 = document.getElementById("inputPassword2");
-function ell(){
-    if (email1.value === email2.value && jelszo1.value === jelszo2.value){
-        button.disabled = false;
-    } else {
-        button.disabled = true;
+button.disabled = true;
+function katt(){
+if (email1.value === email2.value && jelszo1.value === jelszo2.value && email1.value != "" && email2.value != "" && jelszo1.value != "" && jelszo2.value != ""){
+    button.disabled = false;
+
     }
-}
+    
+    if (email1.value != email2.value) {
+      const p = document.createElement("p");
+      const textNode = document.createTextNode("Email nem egyezik.");
+      p.appendChild(textNode);
+      document.body.appendChild(p);
+      }
+  }
