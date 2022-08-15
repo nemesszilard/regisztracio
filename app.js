3f84445a6f50ed56ad0dbb322251f51181dde5b0
nemesszilard <nemes.sziszi@gmail.com>
2022-08-15 12:32:24 +0200

    ellenorzesinputokrraproba

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const button = document.getElementById("gomb");
 button.disabled = true;
-
+var email1 = document.getElementById("inputEmail1");
+var email2 = document.getElementById("inputEmail2");
+var jelszo1 = document.getElementById("inputPassword1");
+var jelszo2 = document.getElementById("inputPassword2");
+function ell(){
+    if (email1.value === email2.value && jelszo1.value === jelszo2.value){
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
+}
