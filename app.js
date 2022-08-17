b6fe6d921f922ef7e8d14536abfd2c4db7e9058d
nemesszilard <nemes.sziszi@gmail.com>
2022-08-17 19:33:41 +0200

    tabolásonkívűl minden kész van

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var email1 = document.getElementById("inputEmail1");
 var email2 = document.getElementById("inputEmail2");
 var jelszo1 = document.getElementById("inputPassword1");
 var jelszo2 = document.getElementById("inputPassword2");
+let gomb;
+
 button.disabled = true;
 function katt(){
-if (email1.value === email2.value && jelszo1.value === jelszo2.value && email1.value != "" && email2.value != "" && jelszo1.value != "" && jelszo2.value != ""){
+if (inputEmail1.value.match(regx) && pass.value.match(/[a-z]+[0-9]+[A-Z]+[!\@\#\$\%\^\&\*\(\)\_\-\+\=\?\>\<\.\,]/) && email1.value === email2.value && jelszo1.value === jelszo2.value && email1.value != "" && email2.value != "" && jelszo1.value != "" && jelszo2.value != ""){
     button.disabled = false;
     }
   }
@@ -24,7 +26,13 @@ function emailell(){
       emailid?.remove();
     }
 }
-
+function jel(){
+  var pass = document.getElementById("inputPassword1");
+  if(pass.value.match(/[a-z]+[0-9]+[A-Z]+[!\@\#\$\%\^\&\*\(\)\_\-\+\=\?\>\<\.\,]/)) {
+    jelszotart?.remove();
+  }else{
+    document.getElementById("jelszotart").innerHTML = "A jelszónak tartalmaznia kell kisbetűt, számot nagybetűt és különlegeskaraktert.";}
+}
 function megjel(){
   if (jelszo1.value != jelszo2.value && jelszo1.value != "" && jelszo2.value != "") {
     document.getElementById("jelszoid").innerHTML = "A két jelszó nem egyezik.";
